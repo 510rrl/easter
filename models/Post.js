222c483bd7df525edea8d0bb2694b6208703cf99
510rrl <rrlopez@uci.edu>
2024-02-22 23:03:18 -0800

    post.js finished

--- models/Post.js
+++ models/Post.js
@@ -3,4 +3,29 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
 module.exports = Post;
