980a856456224f4b62d5948c4aff9d87a45f0de2
510rrl <rrlopez@uci.edu>
2024-02-22 21:45:19 -0800

    comment.js completed

--- models/Comment.js
+++ models/Comment.js
@@ -3,4 +3,25 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
 module.exports = Comment;
