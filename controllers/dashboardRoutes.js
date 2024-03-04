044f509a5b0da204ea8ee7c55d8bc9c95a07a034
510rrl <rrlopez@uci.edu>
2024-03-04 10:51:36 -0800

    Routes file created and dashboard completed

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -21,3 +21,32 @@ router.get('/', withGuard, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/new', withGuard, (req, res) => {
+    res.render('newPost', {
+      dashboard: true,
+      loggedIn: req.session.logged_in,
+    });
+  });
+  
+  router.get('/edit/:id', withGuard, async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id);
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('editPost', {
+          dashboard: true,
+          post,
+          loggedIn: req.session.logged_in,
+        });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
